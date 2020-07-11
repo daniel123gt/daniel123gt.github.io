f467089eae7e0e54f9e102897682a205761ab128
daniel123gt <42387658+daniel123gt@users.noreply.github.com>
2020-07-11 16:25:11 -0500

    Commit 1.10

---
+++ animate.js
@@ -0,0 +1,7 @@
+ScrollReveal().reveal('.aboutTitle');
+ScrollReveal().reveal('.aboutYo');
+ScrollReveal().reveal('.aboutLen');
+ScrollReveal().reveal('.about1');
+ScrollReveal().reveal('.about2');
+ScrollReveal().reveal('.about3');
+ScrollReveal().reveal('.about4');
