9c5c56d5658e003885bed045f22f0d5e2f502b8c
daniel123gt <42387658+daniel123gt@users.noreply.github.com>
2020-07-11 13:38:16 -0500

    Six

--- mail.php
+++ mail.php
@@ -1,3 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Estilos internos -->
+    <link rel="stylesheet" href="styles.css">
+    <!--Favicon-->
+    <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
+    <title>CarDGT</title>
+</head>
+
+<body>
+
 <?php
-    echo "Envio de email";
+    echo"hola";
 ?>
+
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        M.AutoInit();
+    });
+</script>
+</body>
+
+</html>
