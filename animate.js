97633a8bb7b424ad7f11aacd6164c6d7135aa0a9
daniel123gt <42387658+daniel123gt@users.noreply.github.com>
2020-07-11 22:48:52 -0500

    Version 1.12

--- animate.js
+++
@@ -1,7 +0,0 @@
-ScrollReveal().reveal('.aboutTitle');
-ScrollReveal().reveal('.aboutYo');
-ScrollReveal().reveal('.aboutLen');
-ScrollReveal().reveal('.about1');
-ScrollReveal().reveal('.about2');
-ScrollReveal().reveal('.about3');
-ScrollReveal().reveal('.about4');
---
+++ js/animate.js
@@ -0,0 +1,7 @@
+ScrollReveal().reveal('.aboutTitle' , { delay:300 });
+ScrollReveal().reveal('.aboutYo' , { delay:300 });
+ScrollReveal().reveal('.aboutLen' , { delay:300 });
+ScrollReveal().reveal('.about1' , { delay:300 });
+ScrollReveal().reveal('.about2' , { delay:300 });
+ScrollReveal().reveal('.about3' , { delay:300 });
+ScrollReveal().reveal('.about4' , { delay:300 });
